7414d392ad090dbc40a477be2dfedaffd1a4bf70
Josue Odar <josue.odar@inetum.com>
2024-11-15 15:35:02 -0500

    update EntidadDTO

--- appServiceSeguridad/src/main/java/pe/gob/bcrp/dto/EntidadDTO.java
+++ appServiceSeguridad/src/main/java/pe/gob/bcrp/dto/EntidadDTO.java
@@ -1,5 +1,6 @@
 package pe.gob.bcrp.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -26,20 +27,25 @@ public class EntidadDTO {
     @NotNull(message = "documento no puede ser vacio")
     private  Integer  idDocumento;
 
-
+    @JsonIgnore
     private String tipoDocumento;
 
     @NotEmpty(message = "numero de documento no puede ser vacio")
-    @Size(min = 8, max = 20, message = "Número de documento debe tener entre 8 y 20 caracteres.")
+    @Size(min = 8, max = 11, message = "Número de documento debe tener entre 8 y 11 caracteres.")
+    @Pattern(regexp = "[0-9]+",message = "Número de documento debe contener sólo números.")
     private String numeroDocumento;
 
-    @NotEmpty(message = "nombre no puede ser vacio")
+    @Size(max = 100, message = "Ingresar nombre hasta un máximo de 100 caracteres.")
+    @NotEmpty(message = "Nombre no puede ser vacio")
     @Pattern(regexp = "^[a-zñA-ZÑ](\\s?[a-zñA-ZÑ])*$",message = "El nombre solo debe contener letras")
     private String nombre;
 
     @NotEmpty(message = "sigla no puede ser vacio")
     @Pattern(regexp = "^[a-zñA-ZÑ0-9_](\\s?[a-zñA-ZÑ0-9_])*$", message = "Sigla contiene caracteres no permitidos")
     private String sigla;
-    //@NotEmpty(message = "codigo externo no puede ser vacio")
+
+    /*@NotEmpty(message = "codigo externo no puede ser vacio")
+    @Pattern(regexp = "[a-zA-Z0-9]",message = "Código externo sólo debe contenar caracteres alfanúmericos")*/
+    @JsonIgnore
     private String codExterno;
 }
