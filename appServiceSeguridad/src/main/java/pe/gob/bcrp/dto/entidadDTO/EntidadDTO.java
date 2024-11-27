8b23e2f41329739d053fd307f088805e39dd581a
Josue Odar <josue.odar@inetum.com>
2024-11-27 09:46:21 -0500

    change size tipoDocumento

--- appServiceSeguridad/src/main/java/pe/gob/bcrp/dto/entidadDTO/EntidadDTO.java
+++ appServiceSeguridad/src/main/java/pe/gob/bcrp/dto/entidadDTO/EntidadDTO.java
@@ -8,8 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import pe.gob.bcrp.dto.personaDTO.ValidateDni;
-import pe.gob.bcrp.dto.personaDTO.ValidateRuc;
 
 @Data
 @Builder
